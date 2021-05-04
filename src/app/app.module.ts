fe5188534ad9721cdd5dbada44ccaefe83561d4a
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-05-04 16:07:32 -0400

    paths for accounts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { appRoutingModule } from "./app.routing";
+import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from "./home";
-import { HelpComponent } from "./help";
-import {LoginComponent} from "./login";
-import { CheckingAcctComponent } from './checking-acct/checking-acct.component';
-import { SavingsAcctComponent } from './savings-acct/savings-acct.component';
-import { InvestmentAcctComponent } from './investment-acct/investment-acct.component';
+import { HomeComponent } from './home';
+import { HelpComponent } from './help';
+import {LoginComponent} from './login';
+import { CheckingAcctComponent } from './checking-acct';
+import { SavingsAcctComponent } from './savings-acct';
+import { InvestmentAcctComponent } from './investment-acct';
 
 @NgModule({
   declarations: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { HelpComponent } from './help';
+import {CheckingAcctComponent} from './checking-acct';
+import {SavingsAcctComponent} from './savings-acct';
+import {InvestmentAcctComponent} from './investment-acct';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'help', component: HelpComponent},
+  { path: 'checking', component: CheckingAcctComponent},
+  { path: 'savings', component: SavingsAcctComponent},
+  { path: 'investment', component: InvestmentAcctComponent},
 
   // otherwise redirects to home
   { path: '**', redirectTo: ''}
