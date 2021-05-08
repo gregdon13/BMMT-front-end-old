0a66aa4ccac602668ac5e8dd7d2a0382e90b819b
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-05-08 12:51:07 -0400

    transfer page made and looks rudimentary

--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BmmtService} from '../bmmt.service';
 
 @Component({
   selector: 'app-transfer',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent implements OnInit {
+  accounts: any[];
+  accountAction: string;
+  accountFrom: string;
+  accountTo: string;
 
-  constructor() { }
+  constructor(private accountService: BmmtService) {
+    this.getUserAccounts();
+  }
 
   ngOnInit(): void {
+  }
+
+  getUserAccounts(): void {
+    this.accountService.getAccountByUser(23).subscribe(list => this.accounts = list);
   }
 
 }
