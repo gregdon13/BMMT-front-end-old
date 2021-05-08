b1af5bdaa49269c6dc7ad64426365ec7a80d6d12
jorge <jorgetapia0293@gmail.com>
2021-05-08 19:57:40 -0400

    able to display values on help page

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {Faq} from "../models/faq";
 import {BmmtService} from "../bmmt.service";
 
@@ -7,37 +7,39 @@ import {BmmtService} from "../bmmt.service";
   templateUrl: 'help.component.html',
   styleUrls: ['./help.component.css']
 })
-export class HelpComponent implements OnInit{
+export class HelpComponent implements OnInit, DoCheck{
 
-  faq: Faq=new Faq('What is the meaning of life?','42');
-  faqObject: Faq;
-  question: string;
-  answer:string;
+  faqObject: { question: string; answer: string };
+  // id: number;
+  // question: string;
+  // answer:string;
+
 
   constructor(private faqService: BmmtService) {
-    this.getQuestion();
-    this.getAnswer();
+    // this.getQuestion();
+    // this.getAnswer();
   }
   ngOnInit() {
+    this.faqObject = {
+      question: 'What is the meaning of life?',
+      answer: ' Answer: 42! '
+    }
+  }
+
+  ngDoCheck() {
+    console.log('checked');
   }
 
   share() {
-    window.alert(this.getFaqAnswer());
+    window.alert(this.faqObject.answer);
   }
 
-  getQuestion(): void {
-    this.faqService.getFAQById(1).subscribe(faq => this.question = faq.question);
-  }
+  // getQuestion(): void {
+  //   this.faqService.getFAQById(1).subscribe(faq => this.question = faq.question);
+  // }
+  //
+  // getAnswer(): void {
+  //   this.faqService.getFAQById(1).subscribe(faq => this.answer = faq.answer);
+  // }
 
-  getAnswer(): void {
-    this.faqService.getFAQById(1).subscribe(faq => this.answer = faq.answer);
-  }
-
-  getFaqQuestion(): string {
-    return this.faq.Question;
-  }
-
-  getFaqAnswer(): string {
-    return this.faq.Answer;
-  }
 }
